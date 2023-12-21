012d85abd41b20bacb67ae45eed39fd9d293bb7e
oddfar <oddfar@163.com>
2023-12-21 23:00:12 +0800

    feat: 首页访问

---
+++ campus-admin/src/main/java/com/oddfar/campus/admin/controller/system/SysIndexController.java
@@ -0,0 +1,29 @@
+package com.oddfar.campus.admin.controller.system;
+
+import com.oddfar.campus.common.config.CampusConfig;
+import com.oddfar.campus.common.utils.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 首页
+ *
+ * @author oddfar
+ */
+@RestController
+public class SysIndexController {
+    /**
+     * 系统基础配置
+     */
+    @Autowired
+    private CampusConfig campusConfig;
+
+    /**
+     * 访问首页，提示语
+     */
+    @RequestMapping("/")
+    public String index() {
+        return StringUtils.format("欢迎使用{}后台管理框架，当前版本：v{}，请通过前端地址访问。", campusConfig.getName(), campusConfig.getVersion());
+    }
+}
