e74c1dd30726bfc6b44f666130e6599811141805
oddfar <oddfar@163.com>
2024-01-16 13:46:10 +0800

    feat: ip离线查询

--- campus-common/src/main/java/com/oddfar/campus/common/config/properties/CampusConfig.java
+++ campus-common/src/main/java/com/oddfar/campus/common/config/properties/CampusConfig.java
@@ -25,4 +25,29 @@ public class CampusConfig {
      */
     private String version;
 
+    /**
+     * 获取地址开关
+     */
+    private static boolean addressEnabled;
+
+    /**
+     * 是否在线查询ip
+     */
+    private static boolean ipOnlineEnable;
+
+    public static boolean isAddressEnabled() {
+        return addressEnabled;
+    }
+
+    public void setAddressEnabled(boolean addressEnabled) {
+        CampusConfig.addressEnabled = addressEnabled;
+    }
+
+    public static boolean isIpOnlineEnabled() {
+        return ipOnlineEnable;
+    }
+
+    public static void setIpOnlineEnable(boolean ipOnlineEnable) {
+        CampusConfig.ipOnlineEnable = ipOnlineEnable;
+    }
 }
--- campus-common/src/main/java/com/oddfar/campus/common/utils/ip/AddressUtils.java
+++ campus-common/src/main/java/com/oddfar/campus/common/utils/ip/AddressUtils.java
@@ -2,6 +2,7 @@ package com.oddfar.campus.common.utils.ip;
 
 import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONObject;
+import com.oddfar.campus.common.config.properties.CampusConfig;
 import com.oddfar.campus.common.constant.Constants;
 import com.oddfar.campus.common.utils.StringUtils;
 import com.oddfar.campus.common.utils.http.HttpUtils;
@@ -11,7 +12,7 @@ import org.slf4j.LoggerFactory;
 /**
  * 获取地址类
  *
- * @author ruoyi
+ * @author oddfar
  */
 public class AddressUtils {
     private static final Logger log = LoggerFactory.getLogger(AddressUtils.class);
@@ -24,25 +25,32 @@ public class AddressUtils {
 
     public static String getRealAddressByIP(String ip) {
         // 内网不查询
-        if (IpUtils.internalIp(ip)) {
-            return "内网IP";
-        }
-//        if (RuoYiConfig.isAddressEnabled())
-        {
+//        if (IpUtils.internalIp(ip)) {
+//            return "内网IP";
+//        }
+        if (CampusConfig.isAddressEnabled()) {
             try {
-                String rspStr = HttpUtils.sendGet(IP_URL, "ip=" + ip + "&json=true", Constants.GBK);
-                if (StringUtils.isEmpty(rspStr)) {
-                    log.error("获取地理位置异常 {}", ip);
-                    return UNKNOWN;
+                if (CampusConfig.isIpOnlineEnabled()){
+                    return getRealAddressByOnline(ip);
+                }else{
+                    return RegionUtils.getCityInfo(ip);
                 }
-                JSONObject obj = JSON.parseObject(rspStr);
-                String region = obj.getString("pro");
-                String city = obj.getString("city");
-                return String.format("%s %s", region, city);
             } catch (Exception e) {
                 log.error("获取地理位置异常 {}", ip);
             }
         }
         return UNKNOWN;
+    }
+
+    public static String getRealAddressByOnline(String ip) {
+        String rspStr = HttpUtils.sendGet(IP_URL, "ip=" + ip + "&json=true", Constants.GBK);
+        if (StringUtils.isEmpty(rspStr)) {
+            log.error("获取地理位置异常 {}", ip);
+            return UNKNOWN;
+        }
+        JSONObject obj = JSON.parseObject(rspStr);
+        String region = obj.getString("pro");
+        String city = obj.getString("city");
+        return String.format("%s %s", region, city);
     }
 }
